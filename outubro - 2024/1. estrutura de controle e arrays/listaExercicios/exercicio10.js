639b1f919ebef6e8293f93f7606a6dc98aa505f4
ssnaligia <ana.ligia@aluno.ifsp.edu.br>
2024-10-11 21:30:17 -0300

    exercicio10

--- outubro - 2024/1. estrutura de controle e arrays/listaExercicios/exercicio10.js
+++ outubro - 2024/1. estrutura de controle e arrays/listaExercicios/exercicio10.js
@@ -1,3 +1,21 @@
-const texto1 = 'Juca comprou fafarinha na memercearia e papagou 4 reais o quilo. A mamae de Juca pediu para ele comprar mamais 2 quilos.';
+function traduzirTexto(texto) {
+    const palavras = texto.split(' '); 
+    const resultado = palavras.map(palavra => {
+        if (palavra.length >= 2) { 
+            const primeiraSilaba = palavra.slice(0, 2); //pega a primeira sílaba
+            
+            if (palavra.startsWith(primeiraSilaba + primeiraSilaba)) { //verifica se a palavra já tem a sílaba repetida
+                return palavra.slice(0, 2) + palavra.slice(4); //se a palavra começa com a sílaba repetida, remove a repetição
+            }
+        }
+        return palavra; 
+    });
 
+    return resultado.join(' '); //junta as palavras novamente em um texto
+}
+
+const texto1 = 'Juca comprou fafarinha na memercearia e papagou 4 reais o quilo. A mamae de Juca pediu para ele comprar mamais 2 quilos.';
 const texto2 = 'O papassarinho vovoou para bem longe.';
+
+console.log(traduzirTexto(texto1));
+console.log(traduzirTexto(texto2));
